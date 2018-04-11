a076d85d64b1a49c02caa0500a7a4ea6e55c4508
Nicolo Ereni <nicolo.ereni@nozominetworks.com>
2018-04-11 08:14:30 +0200

    add string exercise

---
+++ src/SubstringExercise.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SubstringExercise {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        int stringLength = s.length();
+
+        if((1 <= stringLength) && (100 >= stringLength) && (end > start) && (start >= 0) && (end <= stringLength))
+        {
+            System.out.println(s.substring(start, end -1));
+        }
+    }
+}
